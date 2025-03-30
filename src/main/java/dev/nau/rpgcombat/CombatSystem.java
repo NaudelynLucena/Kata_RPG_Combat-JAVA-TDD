9b3426233f5eb776a81e2a42b3e01934563d3000
NaudelynLucena <naulunau@gmail.com>
2025-03-31 01:24:50 +0200

    feat: prevent a character from attacking themselves. test passes

--- src/main/java/dev/nau/rpgcombat/CombatSystem.java
+++ src/main/java/dev/nau/rpgcombat/CombatSystem.java
@@ -4,6 +4,8 @@ public class CombatSystem {
     public static boolean attack(Character attacker, Character target, int damage) {
         if (!attacker.isAlive() || !target.isAlive())
             return false;
+        if (attacker == target)
+            return false;
 
         target.receiveDamage(damage);
         return true;
