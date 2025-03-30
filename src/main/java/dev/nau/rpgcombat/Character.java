46bab48aaf3cfdd8b2316b6a59e247ce0da70790
NaudelynLucena <naulunau@gmail.com>
2025-03-30 15:08:19 +0200

    Feat: Implement character creation with initial values, tests pass

---
+++ src/main/java/dev/nau/rpgcombat/Character.java
@@ -0,0 +1,23 @@
+package dev.nau.rpgcombat;
+
+public class Character {
+    private String name;
+    private int health;
+    private int level;
+    private boolean alive;
+
+    public Character(String name) {
+        this.name = name;
+        this.health = 1000;
+        this.level = 1;
+        this.alive = true;
+    }
+
+    public String getName() {return name;}
+
+    public int getHealth() {return health;}
+
+    public int getLevel() {return level;}
+
+    public boolean isAlive() {return alive;}
+}
