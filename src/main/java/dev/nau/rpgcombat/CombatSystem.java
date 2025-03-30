2facd120f8cba7d4036a23093ed0e99224e449dd
NaudelynLucena <naulunau@gmail.com>
2025-03-31 01:18:35 +0200

    Feat: Added functionality to prevent attacking dead characters and preventing them from being attacked. Test passes

--- src/main/java/dev/nau/rpgcombat/CombatSystem.java
+++ src/main/java/dev/nau/rpgcombat/CombatSystem.java
@@ -2,8 +2,9 @@ package dev.nau.rpgcombat;
 
 public class CombatSystem {
     public static boolean attack(Character attacker, Character target, int damage) {
-        if (!attacker.isAlive())
+        if (!attacker.isAlive() || !target.isAlive())
             return false;
+
         target.receiveDamage(damage);
         return true;
     }
